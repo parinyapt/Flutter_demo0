9ceb8f5d8dce4643ea94f3405705c5f5f4243cbb
parinyapt <parinyapt99@gmail.com>
2023-02-04 14:11:01 +0700

    add login page

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,9 @@
 // import 'package:flutter/material.dart'; // android ui
 // import 'package:flutter/widgets.dart'; // according to os
 
+import 'package:demo0/src/app.dart';
 import 'package:flutter/material.dart';
 
 main() {
   runApp(App());
 }
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        child: Text("SWU"),
-      ),
-    );
-  }
-}
-
