0526a953c1801bd30652d73138b82f7f0aaa1c17
parinyapt <parinyapt99@gmail.com>
2023-02-04 12:02:30 +0700

    first run app

---
+++ android/app/src/main/kotlin/com/swu/flutter/demo0/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.swu.flutter.demo0
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
